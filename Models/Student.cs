a4b0f6192b4b644fd78e3027518c2afec27037e1
Keshav Raj Choudhary <Keshavrajchaudhary@t-a-g.net>
2023-10-17 15:14:12 +0530

    added get

---
+++ Models/Student.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazorUI.Models
+{
+    public class Student
+    {
+        public string StudentId { get; set; }
+        public string? Gender { get; set; }
+        public string? Nationality { get; set; }
+        public string? PlaceOfBirth { get; set; }
+        public string? StageId { get; set; }
+        public string? GradeId { get; set; }
+        public string? SectionId { get; set; }
+        public string? Topic { get; set; }
+        public string? Semester { get; set; }
+        public string? Relation { get; set; }
+        public int? RaisedHands { get; set; }
+        public int? VisitedResources { get; set; }
+        public int? AnnouncementsView { get; set; }
+        public int? Discussion { get; set; }
+        public string? ParentAnsweringSurvey { get; set; }
+        public string? ParentSchoolSatisfaction { get; set; }
+        public string? StudentAbsenceDays { get; set; }
+        public int? StudentMarks { get; set; }
+        public string? Class { get; set; }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+using BlazorUI;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7124/api/StudentApi") });
+
+await builder.Build().RunAsync();
+
